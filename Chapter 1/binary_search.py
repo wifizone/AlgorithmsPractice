a3faa3e15434ce2c0e6a7cb35d5cbd8a36ae590a
Anton Poluianov <poluyanov80@gmail.com>
2018-10-31 17:08:28 +0300

    Updated binary search

--- Chapter 1/binary_search.py
+++ Chapter 1/binary_search.py
@@ -1,26 +1,26 @@
-def search(array, value):
-	low = 0
-	high = len(array) - 1
-	
-	while(low <= high):
-		mid = int((high + low) / 2)
-		if array[mid] == value:
-			return mid
-		elif array[mid] > value:
-			high = mid - 1
-		else:
-			low = mid + 1
-	return -1
+class BinarySearchClass:
 
+    @staticmethod
+    def binarySearch(value, sortedArray):
+        low = 0
+        high = len(sortedArray) - 1
 
-a = [1, 2, 3, 4, 5, 6, 7, 8]
+        while (high >= low):
+            mid = int((high + low) / 2)
+            if (value > sortedArray[mid]):
+                low = mid + 1
+            elif (value < sortedArray[mid]):
+                high = mid - 1
+            else:
+                return mid
 
-print(search(a, 1))
-print(search(a, 2))
-print(search(a, 3))
-print(search(a, 4))
-print(search(a, 5))
-print(search(a, 6))
-print(search(a, 7))
-print(search(a, 8))
-print(search(a, 9))
+        return -1
+
+sortedArray = [2, 6, 8, 10, 14, 18, 20, 40, 43, 49, 63, 70]
+
+print(BinarySearchClass.binarySearch(10, sortedArray))
+print(BinarySearchClass.binarySearch(65, sortedArray))
+print(BinarySearchClass.binarySearch(2, sortedArray))
+print(BinarySearchClass.binarySearch(63, sortedArray))
+print(BinarySearchClass.binarySearch(40, sortedArray))
+
